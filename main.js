661b4d777d64a01a5bda3dd02fe5c78072f088a4
BoubakerSaif <127789628+BoubakerSaif@users.noreply.github.com>
2024-03-12 21:38:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+/*
+Create Element
+createAttribute
+appendChild
+*/
+
+// let myElement = document.createElement("div");
+// let myAtrr = document.createAttribute("data-custom");
+// let myText = document.createTextNode("Product One");
+
+// myElement.className = "product";
+// myElement.setAttributeNode(myAtrr);
+// myElement.setAttribute("data-testing", "test");
+
+// // Apending Text to Element
+// myElement.appendChild(myText);
+
+// // Apending Element to Body
+// document.body.appendChild(myElement);
+
+// console.log(myElement);
+
+/*
+Children
+childNodes
+firstChild
+lastChild
+firstElementChild
+lastElementChild
+
+*/
+
+// let myElement = document.querySelector("div");
+
+// console.log(myElement);
+// console.log(myElement.children);
+// console.log(myElement.children[0]);
+// console.log(myElement.childNodes);
+// console.log(myElement.childNodes[0]);
+
+// console.log(myElement.firstChild);
+// console.log(myElement.lastChild);
+
+// console.log(myElement.firstElementChild);
+// console.log(myElement.lastElementChild);
+
+// let myBtn = document.getElementById("btn");
+
+// myBtn.onclick = function () {
+//   let myP = document.querySelector("p");
+//   console.log(myP);
+
+//   myP.innerHTML = Number(myP.innerHTML) + 1;
+// };
+
+// myBtn.oncontextmenu = function () {
+//   console.log("Clicked");
+// };
+
+// myBtn.onmouseenter = function () {
+//   console.log("Clicked");
+// };
+
+// myBtn.onmouseleave = function () {
+//   console.log("Clicked");
+// };
